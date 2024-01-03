14a8d987570d53c4059ddb66f97731e5dcc78a71
ameduam <lrns1-edu@outlook.com>
2024-01-03 19:53:33 +0800

    [Fanshen] N/A feat: "synchronized" lock feature and optimization demo

---
+++ Synchronized/src/main/java/com/self/layout/instance/ObjectHeaderTest.java
@@ -0,0 +1,17 @@
+package com.self.layout.instance;
+
+/**
+ * @author : lrns1
+ * @version : 1.0
+ * @date : Created in 2024/1/3
+ * @description :
+ */
+public class ObjectHeaderTest {
+    private static Object object = new Object();
+
+    public Object object1 = new Object();
+    private static int[] arr = new int[0];
+    private static int a = 1;
+    private static int b;
+    public int c = 1;
+}
